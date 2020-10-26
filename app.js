df1267ae4dda90c99dd3c9d2830106c074d587d5
Justyna Chrostek <justyna.maria.chrostek@gmail.com>
2020-10-26 14:18:22 +0100

    Create summary modal

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
-const open = document.getElementById("open");
-const close = document.getElementById("close");
-const modal = document.getElementById("modal");
+const open = document.querySelectorAll(".open");
+const openSummary = document.getElementById("summary-container");
+const openCustomDrink = document.getElementById("open-custom-drink");
+const close = document.querySelectorAll(".close");
+const closeCustomDrink = document.getElementById("close-custom-drink");
+const closeCustomDrinkIMG = document.querySelector("#close-custom-drink img");
+const closeSummary = document.getElementById("close-summary");
+const closeSummaryIMG = document.querySelector("#close-summary img");
+const modalCustomDrink = document.getElementById("modal-custom-drink");
+const modalSummary = document.getElementById("modal-summary");
+const amountBtn = document.querySelectorAll(".amount-btn");
 
-
-open.addEventListener('click', () => {
-   modal.classList.add("show-modal")
+open.forEach(item => {
+  item.addEventListener("click", e => {
+    if (e.target.parentElement == openSummary || e.target == openSummary) {
+      modalSummary.classList.add("show-modal");
+    } else if (e.target == openCustomDrink) {
+      modalCustomDrink.classList.add("show-modal");
+    }
+  });
 });
 
-close.addEventListener('click', () => {
-   modal.classList.remove("show-modal")
+close.forEach(item => {
+  item.addEventListener("click", e => {
+    if (e.target == closeSummary || e.target.parentElement == closeSummary) {
+      modalSummary.classList.remove("show-modal");
+    } else if (
+      e.target == closeCustomDrink ||
+      e.target.parentElement == closeCustomDrink
+    ) {
+      modalCustomDrink.classList.remove("show-modal");
+    }
+  });
 });
 
-window.addEventListener("click", e => e.target == modal ? modal.classList.remove('show-modal') : false )
+window.addEventListener("click", e => {
+  if (e.target == modalSummary) {
+    modalSummary.classList.remove("show-modal");
+  } else if (e.target == modalCustomDrink) {
+    modalCustomDrink.classList.remove("show-modal");
+  }
+});
+
+amountBtn.forEach(btn => {
+  btn.addEventListener("click", addToSummary);
+});
