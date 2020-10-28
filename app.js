fe1413d37ed11c9e6413ff4fae79f9560e871cf7
Justyna Chrostek <justyna.maria.chrostek@gmail.com>
2020-10-28 13:17:37 +0100

    Create class Drink & class DrinkCounter

--- app.js
+++ app.js
@@ -8,7 +8,42 @@ const closeSummary = document.getElementById("close-summary");
 const closeSummaryIMG = document.querySelector("#close-summary img");
 const modalCustomDrink = document.getElementById("modal-custom-drink");
 const modalSummary = document.getElementById("modal-summary");
-const amountBtn = document.querySelectorAll(".amount-btn");
+
+class Drink {
+  constructor(mlAmount, percentAmount, timeStamp) {
+    this.mlAmount = mlAmount;
+    this.percentAmount = percentAmount;
+    this.timeStamp = timeStamp;
+  }
+
+  getAlcoholInGrams() {
+    return (8 * this.mlAmount * this.percentAmount) / 1000;
+  }
+}
+
+class DrinkCounter {
+  constructor() {
+    this.drinks = [];
+    this.total = 0;
+  }
+
+  drinksFromLast7Days() {
+    function drinkCondition(drink) {
+      let startDate = new Date();
+      startDate.setDate(startDate.getDate() - 7);
+      return drink.timeStamp >= startDate;
+    }
+    let sevenDrinks = this.drinks.filter(drinkCondition);
+    return sevenDrinks;
+  }
+
+  addDrink(mlAmount, percentAmount, timeStamp) {
+    let drink = new Drink(mlAmount, percentAmount, timeStamp);
+    this.drinks.push(drink);
+    let alcoholInGrams = drink.getAlcoholInGrams();
+    this.total += alcoholInGrams;
+  }
+}
 
 open.forEach(item => {
   item.addEventListener("click", e => {
@@ -39,8 +74,4 @@ window.addEventListener("click", e => {
   } else if (e.target == modalCustomDrink) {
     modalCustomDrink.classList.remove("show-modal");
   }
-});
-
-amountBtn.forEach(btn => {
-  btn.addEventListener("click", addToSummary);
 });
