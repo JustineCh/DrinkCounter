b43507e02580287b5a04fbf6094bc49d2184a692
Justyna Chrostek <justyna.maria.chrostek@gmail.com>
2020-11-05 11:29:33 +0100

    Update of DrinkCounter class & create isAboveLimit funcs

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const closeSummary = document.getElementById("close-summary");
 const closeSummaryIMG = document.querySelector("#close-summary img");
 const modalCustomDrink = document.getElementById("modal-custom-drink");
 const modalSummary = document.getElementById("modal-summary");
+const smallAmountBtn = document.getElementById("small");
+const mediumAmountBtn = document.getElementById("medium");
+const largeAmountBtn = document.getElementById("large");
 
 class Drink {
   constructor(mlAmount, percentAmount, timeStamp) {
@@ -22,39 +25,75 @@ class Drink {
 }
 
 class DrinkCounter {
-  constructor() {
+  constructor(
+    periodInDays = 7, 
+    softLimit = 12*14, // 12 standard drinks 14g each
+    hardLimit = 20*14 // 20 standard drinks 14g each 
+    ) {
     this.drinks = [];
+    this.periodInDays = periodInDays;
+    this.softLimit = softLimit;
+    this.hardLimit = hardLimit;
   }
 
-  drinksFromLastDays(days) {
+  drinksFromLastDays() {
+    let periodInDays = this.periodInDays;
     function drinkCondition(drink) {
       let startDate = new Date();
-      startDate.setDate(startDate.getDate() - days);
+      startDate.setDate(startDate.getDate() - periodInDays);
       return drink.timeStamp >= startDate;
     }
     return this.drinks.filter(drinkCondition);
   }
 
-  sumOfAlcoholInGrams(days) {
-    const drinksList = this.drinksFromLastDays(days);
+  sumOfAlcoholInGrams() {
+    const drinksList = this.drinksFromLastDays(this.periodInDays);
     let total = 0;
     drinksList.forEach(drink => {
       total += drink.getAlcoholInGrams();
     });
-    if(total <= 238){
-      console.log('under the limit')
-      return total
-    } else {
-      console.log('over the limit')
-      return total
-    }
+    return total
   }
+
+  isAboveSoftLimit(sum) {
+   return sum <= this.softLimit
+  }
+
+  isAboveHardLimit(sum) {
+    return sum <= this.hardLimit
+   }
 
   addDrink(mlAmount, percentAmount, timeStamp) {
     let drink = new Drink(mlAmount, percentAmount, timeStamp);
     this.drinks.push(drink);
   }
 }
+
+const drinkCounter = new DrinkCounter();
+
+smallAmountBtn.addEventListener("click", () => {
+  drinkCounter.addDrink(20, 40, new Date());
+  let sum = drinkCounter.sumOfAlcoholInGrams(this.periodInDays)
+  drinkCounter.isAboveSoftLimit(sum)
+  drinkCounter.isAboveHardLimit(sum)
+  console.log(drinkCounter.drinks)
+});
+
+mediumAmountBtn.addEventListener("click", () => {
+  drinkCounter.addDrink(40, 40, new Date());
+  let sum = drinkCounter.sumOfAlcoholInGrams(this.periodInDays)
+  drinkCounter.isAboveSoftLimit(sum)
+  drinkCounter.isAboveHardLimit(sum)
+  console.log(drinkCounter.drinks)
+});
+
+largeAmountBtn.addEventListener("click", () => {
+  drinkCounter.addDrink(60, 40, new Date());
+  let sum = drinkCounter.sumOfAlcoholInGrams(this.periodInDays)
+  drinkCounter.isAboveSoftLimit(sum)
+  drinkCounter.isAboveHardLimit(sum)
+  console.log(drinkCounter.drinks)
+});
 
 open.forEach(item => {
   item.addEventListener("click", e => {
