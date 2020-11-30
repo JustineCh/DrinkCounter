4c9967dd1c010885238cb0a7fbcf231b143a3d32
Justyna Chrostek <justyna.maria.chrostek@gmail.com>
2020-11-30 11:44:45 +0100

    Format update & create updateLastDrink func

--- ui.js
+++ ui.js
@@ -1,15 +1,23 @@
 class UI {
+  softLimitColorChange() {
+    document
+      .getElementById("summary-container")
+      .classList.add("soft-limit-color");
+  }
 
-   softLimitColorChange(){
-      document.getElementById("summary-container").classList.add("soft-limit-color")
-   }
+  hardLimitColorChange() {
+    document
+      .getElementById("summary-container")
+      .classList.add("hard-limit-color");
+  }
 
-   hardLimitColorChange(){
-      document.getElementById("summary-container").classList.add("hard-limit-color")
-   }
+  updateTotal(value) {
+    document.getElementById("summary").innerHTML = value;
+  }
 
-   updateTotal(value){
-      document.getElementById("summary").innerHTML = value
-   }
-
+  updateLastDrink(lastDrink) {
+    document.getElementById(
+      "last"
+    ).innerHTML = `${lastDrink.mlAmount} ml <br> of <br> ${lastDrink.percentAmount} % alcohol`;
+  }
 }
